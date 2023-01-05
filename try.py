58a77eec22253feefc3f433683e1d23b5b5b5538
dhineshanp <122012517+dhineshanp@users.noreply.github.com>
2023-01-05 19:17:36 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,10 @@
+
+while True:
+    a=int(input("Enter your value :"))
+    if a==10:
+        break
+        print("pass")
+    elif a<10:
+        print("less than")
+    elif a>10:
+        print("greathan")
